384be5b32991da6562605fff99005ee08cf8ba93
xiaban <980892894@qq.com>
2025-03-27 01:43:27 +0800

    refactor: 移除冗余的VO和View类以简化代码结构
    移除不再使用的VO和View类，减少代码冗余，提高代码可维护性。这些类在项目中已不再使用，删除它们可以简化代码库并减少潜在的维护负担。

--- src/main/java/com/dao/ExamQuestionDao.java
+++ src/main/java/com/dao/ExamQuestionDao.java
@@ -2,13 +2,6 @@ package com.dao;
 
 import com.entity.ExamQuestionEntity;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import java.util.List;
-import com.baomidou.mybatisplus.core.conditions.Wrapper;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-
-import org.apache.ibatis.annotations.Param;
-import com.entity.vo.ExamQuestionVO;
-import com.entity.view.ExamQuestionView;
 
 /**
  * 试题表
